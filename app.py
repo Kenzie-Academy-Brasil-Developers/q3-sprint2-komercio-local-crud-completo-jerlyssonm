d0495228858899b9bb995bcf91e914497679a6f5
JerlyssonM <jerlyssonm@gmail.com>
2021-12-14 22:18:29 -0300

    rotas e funções funcionando de acordo

--- app.py
+++ app.py
@@ -1,33 +1,51 @@
 # Seu código aqui
-produtos = [
-    {"id": 1, "name": "sabonete", "price": 5.99},
-    {"id": 2, "name": "perfume", "price": 39.90},
-    {"id": 3, "name": "tapete", "price": 10.30},
-    {"id": 4, "name": "tunica", "price": 19.29},
-    {"id": 5, "name": "chuveiro", "price": 119.19},
-    {"id": 6, "name": "arroz", "price": 30.10},
-    {"id": 7, "name": "oleo de cozinha", "price": 11.15},
-    {"id": 8, "name": "carne moida", "price": 39.90},
-    {"id": 9, "name": "bola", "price": 25.99},
-    {"id": 10, "name": "cantil", "price": 55.99},
-    {"id": 11, "name": "copo", "price": 5.99},
-    {"id": 12, "name": "panela", "price": 25.99},
-    {"id": 13, "name": "prato", "price": 10.99},
-    {"id": 14, "name": "açucar", "price": 7.99},
-    {"id": 15, "name": "sal", "price": 5.99},
-    {"id": 16, "name": "pipoca", "price": 3.14},
-    {"id": 17, "name": "sabonete", "price": 5.99},
-    {"id": 18, "name": "miojo", "price": 2.39},
-    {"id": 19, "name": "alface", "price": 3.99},
-    {"id": 20, "name": "tomate", "price": 9.99},
-    {"id": 21, "name": "macarrao", "price": 6.40},
-    {"id": 22, "name": "mesa", "price": 115.99},
-    {"id": 23, "name": "cadeira gamer", "price": 445.99},
-    {"id": 24, "name": "mouse gamer", "price": 215.99},
-    {"id": 25, "name": "tv", "price": 995.99},
-    {"id": 26, "name": "liquidificador", "price": 65.99},
-    {"id": 27, "name": "furadeira", "price": 99.15},
-    {"id": 28, "name": "ferro de passar", "price": 55.80},
-    {"id": 29, "name": "coberta", "price": 55.99},
-    {"id": 30, "name": "sofa", "price": 600.15}
-]
+from flask import Flask, json, request, jsonify
+from produtos_db import produtos
+
+app = Flask(__name__)
+
+@app.get('/')
+def greeting():
+    message = {"message": "Entrega 5 - Komercio Local"}
+    return message
+
+@app.route('/products', methods=['POST','GET'])
+def list_products():
+    if request.method == 'POST':
+        data = request.get_json()
+        new_product = data.get('name')
+        produtos.append(data)
+        return {'message': f"O Produto {new_product} foi adcionado em estoque!"}, 201
+    elif request.method == 'GET':
+        return jsonify(produtos), 200
+
+@app.get("/products/<int:product_id>")
+def get(product_id: int):
+    quantidade_de_produtos = len(produtos)
+    for x in produtos:
+        if quantidade_de_produtos < product_id or product_id < 1:
+            return {'message': 'Produto não existe'}
+        elif product_id == x['id']:
+            x = [x]
+            return jsonify(x), 200
+
+@app.route('/products/<int:product_id>', methods=["PATCH","PUT"])
+def update(product_id: int):
+    quantidade_de_produtos = len(produtos)
+    for x in produtos:
+        if quantidade_de_produtos < product_id or product_id < 1:
+            return {'message': 'Produto não existe'},204
+        elif product_id == x['id']:
+            data= request.get_json()
+            x['name'] =data['name']
+            return jsonify(), 204   
+
+@app.delete('/products/<int:product_id>')
+def delete(product_id):
+    quantidade_de_produtos = len(produtos)
+    for indice, product in enumerate(produtos):
+        if quantidade_de_produtos < product_id or product_id < 1:
+            return {'message': 'Produto não existe'},204
+        elif product_id == product['id']:
+            del produtos[indice]
+            return jsonify(), 204      
